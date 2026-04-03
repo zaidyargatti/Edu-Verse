5f92f4f1670cf74f736793afad6b7cd76216c68c
zaid Yargatti <9928zaid@gmail.com>
2026-04-03 15:48:05 +0530

    Backend deployed link added to frontend

--- frontend/src/api/Axios.jsx
+++ frontend/src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://edu-verse-63ox.onrender.com/api',
 })
 
 api.interceptors.request.use((req)=>{
